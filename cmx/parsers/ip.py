a7cf3f5c3a6cc6dd20d4b59de99a3e9a182ca455
awsm <ryan.preston.01@gmail.com>
2019-08-26 13:24:18 -0500

    Update ip.py

--- cmx/parsers/ip.py
+++ cmx/parsers/ip.py
@@ -11,4 +11,10 @@ def parse_targets(target):
     if netaddr.ip.nmap.valid_nmap_range(target):
         return list(netaddr.ip.nmap.iter_nmap_range(target))
     else:
-        return list(socket.gethostbyname(target.strip()))
+        try:
+            t = socket.gethostbyname(target.strip())
+        except:
+            print("Error getting IP Info for {}".format(target.strip()))
+            return list()
+        return [t]
+
