139334063ed2d101e3ec57b280b5c32d68e87603
awsm <ryan.preston.01@gmail.com>
2019-08-26 15:00:37 -0500

    Update smb.py

--- cmx/protocols/smb.py
+++ cmx/protocols/smb.py
@@ -1614,7 +1614,7 @@ class smb(connection):
                                     #https://github.com/SecureAuthCorp/impacket/impacket/dcerpc/v5/samr.py #2.2.7.29 SAMPR_USER_INFO_BUFFER
 
                                 #self.logger.results('Computername: {:<25}  rid: {}'.format(user['Name'], user['RelativeId']))
-                                self.logger.highlight('Computer: {:<25}  rid: {}'.format(user['Name'], user['RelativeId']))
+                                self.logger.highlight('{:<20} isDC=False  rid: {}'.format(user['Name'], user['RelativeId']))
                                 info = samr.hSamrQueryInformationUser2(dce, r['UserHandle'],samr.USER_INFORMATION_CLASS.UserAllInformation)
                                 logging.debug('Dump of hSamrQueryInformationUser2 response:')
                                 if self.debug:
@@ -1631,7 +1631,7 @@ class smb(connection):
                                 # r has the clases defined here: 
                                     #https://github.com/SecureAuthCorp/impacket/impacket/dcerpc/v5/samr.py #2.2.7.29 SAMPR_USER_INFO_BUFFER
 
-                                self.logger.results('Computername: {:<25}  rid: {}'.format(user['Name'], user['RelativeId']))
+                                self.logger.highlight('{:<20} isDC=True  rid: {}'.format(user['Name'], user['RelativeId']))
                                 info = samr.hSamrQueryInformationUser2(dce, r['UserHandle'],samr.USER_INFORMATION_CLASS.UserAllInformation)
                                 logging.debug('Dump of hSamrQueryInformationUser2 response:')
                                 if self.debug:
