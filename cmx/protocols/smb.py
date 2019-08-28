c57bebcc1c888f34ab97a347d9c28846f52d2e04
awsm <ryan.preston.01@gmail.com>
2019-08-28 18:12:43 -0500

    Update smb.py

--- cmx/protocols/smb.py
+++ cmx/protocols/smb.py
@@ -1526,8 +1526,10 @@ class smb(connection):
                         print('here')
                         resp.dump()
                         print('ended')
+                        pdb.set_trace()
                         rid = resp['RelativeIds']['Element'][0]['Data']
                         print(rid)
+
                         self.logger.success('Domain Users enumerated')
                         self.logger.highlight("     {} Domain User Accounts".format(tmpdomain))
                         while status == STATUS_MORE_ENTRIES:
