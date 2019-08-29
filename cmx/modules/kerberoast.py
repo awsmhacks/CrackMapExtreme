705684334a10eb8346d270851fafa6960f80eebd
awsmhacks <ripr4p@gmail.com>
2019-08-29 13:33:48 -0500

    Update kerberoast.py

--- cmx/modules/kerberoast.py
+++ cmx/modules/kerberoast.py
@@ -63,23 +63,11 @@ cmx --verbose smb 192.168.1.1 -u username -p password -M kerberoast -mo '-Creden
         response.stop_tracking_host()
 
         if len(data):
-            if self.command.find('sekurlsa::logonpasswords') != -1:
-                creds = None
-                if len(creds):
-                    for cred_set in creds:
-                        credtype, domain, username, password,_,_ = cred_set
-                        # Get the hostid from the DB
-                        hostid = context.db.get_computers(response.client_address[0])[0][0]
-                        context.db.add_credential(credtype, domain, username, password, pillaged_from=hostid)
-                        context.log.highlight('{}\\{}:{}'.format(domain, username, password))
-
-                    context.log.success("Added {} credential(s) to the database".format(highlight(len(creds))))
-            else:
-                context.log.highlight(data)
-
-                #cant use ':' in filename cause of windows 
-            log_name = 'Kerberoasted_{}_on_{}.log'.format(response.client_address[0], datetime.now().strftime("%b.%d.%y_at_%H%M"))
-            write_log(str(data, 'utf-8'), log_name)
-            context.log.info("Saved raw Kerberoast output to {}/{}".format(cfg.LOGS_PATH,log_name))
+            #buf = StringIO(data).readlines()
+            lines = data.decode().split("             ")
+            #pdb.set_trace() #stuff = iter(data.decode().split())    for thing in stuff: print(thing) 
+            for line in lines:
+                #line = line.replace('\r\n', '\n').strip()
+                context.log.highlight(line)
         else:
             context.log.info("No Results ¯\\_('_')_/¯")
