9ec281346cec331589597420daee68ed572bba2a
awsm <ryan.preston.01@gmail.com>
2019-09-02 16:03:45 -0500

    Update smb.py

--- cmx/protocols/smb.py
+++ cmx/protocols/smb.py
@@ -1976,9 +1976,8 @@ class smb(connection):
         if self.args.log:
             ctime = datetime.now().strftime("%b.%d.%y_at_%H%M")
             log_name = 'GroupMembers_of_{}_on_{}.log'.format(targetGroup, ctime)
-            print (groupLog)
-            #write_log(''.join(groupLog), log_name)
-            #self.logger.info("Saved Group Members output to {}/{}".format(cfg.LOGS_PATH,log_name))
+            write_log(groupLog , log_name)
+            self.logger.info("Saved Group Members output to {}/{}".format(cfg.LOGS_PATH,log_name))
 
         self.logger.announce('Finished Group Enum')
         return list()
