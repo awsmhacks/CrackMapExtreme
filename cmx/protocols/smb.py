ab1aa7b3e9d46c92e4cdfab4ec2cd5c81158608a
awsm <ryan.preston.01@gmail.com>
2019-09-02 17:02:51 -0500

    Update smb.py

--- cmx/protocols/smb.py
+++ cmx/protocols/smb.py
@@ -1548,7 +1548,7 @@ class smb(connection):
                             #self.logger.results('Groupname: {:<30}  membercount: {}'.format(group['Name'], info['Buffer']['General']['MemberCount']))
                             #print('')
                             self.logger.highlight('{:<30}  membercount: {}'.format(group['Name'], info['Buffer']['General']['MemberCount']))
-                            groupLog += '{:<30}  membercount: {}'.format(group['Name'], info['Buffer']['General']['MemberCount'])
+                            groupLog += '{:<30}  membercount: {}\n'.format(group['Name'], info['Buffer']['General']['MemberCount'])
 
                             #groupResp = samr.hSamrGetMembersInGroup(dce, r['GroupHandle'])
                             #logging.debug('Dump of hSamrGetMembersInGroup response:')
