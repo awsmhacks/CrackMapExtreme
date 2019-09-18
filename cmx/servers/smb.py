faa20f4ecc329bbb5c2ead7024f5f8f98fa385cd
awsm <ryan.preston.01@gmail.com>
2019-09-18 18:12:53 -0500

    Update smb.py

--- cmx/servers/smb.py
+++ cmx/servers/smb.py
@@ -30,8 +30,8 @@ class CMXSMBServer(threading.Thread):
             
             # username can be a list of users, we only gonna make this work if you pass 1 user for now...
             self.server.addCredential(username[0], 0, lmhash, nthash)
-            self.server.addCredential(computer, 1, '', '')
-            self.server.addCredential('', 2, '', '')
+            self.server.addCredential(computer, 0, '', '')
+            self.server.addCredential('', 0, lmhash, lmhash)
 
             # Here you can set a custom SMB challenge in hex format, If empty defaults to '4141414141414141'
             # e.g. server.setSMBChallenge('12345678abcdef00')
